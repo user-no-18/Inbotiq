db4d39ddef12657eb2a8f8891ccedfab843c6a6e
user-no-18 <ece2023110@rcciit.org.in>
2025-11-14 12:57:44 +0530

    feat: add environment configuration files and streamline MongoDB connection

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,19 +1,15 @@
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        
-        console.log("MongoDB connected");
-        console.log(`Database: ${mongoose.connection.name}`);
-        console.log(`Host: ${mongoose.connection.host}`);
-        console.log(`Port: ${mongoose.connection.port}`);
-        console.log("uff shanti !!")
-    } catch (error) {
-        console.error("MongoDB connection failed:", error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("MongoDB connected");
+    console.log(`Database: ${mongoose.connection.name}`);
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
